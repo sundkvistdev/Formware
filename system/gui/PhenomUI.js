565b1907184c047510668bd5165548ccd2c9075e
sundkvistdev <lundish420flou@gmail.com>
2026-04-20 09:22:44 +0300

    Added basic element construction function

--- system/gui/PhenomUI.js
+++ system/gui/PhenomUI.js
@@ -14,8 +14,32 @@ export const PhenomUI = () => {
                 (v) => void (states[index] = v),
             ];
         },
-        createUiElement: () => {
-
+        /**
+         * @param {Object} param0
+         * @param {string} param0.type
+         * @param {string} param0.className
+         * @param {string} param0.id
+         * @param {CSSStyleDeclaration} param0.style
+         * @param {HTMLElement[]} param0.children
+         * @returns {HTMLElement}
+         */
+        constructEl: ({
+            type = "div",
+            className = "",
+            id = "",
+            style = {},
+            children = [],
+        } = {}) => {
+            const element = document.createElement(type);
+            element.className = className;
+            element.id = id;
+            Object.entries(style).forEach((k, v) => {
+                element.style[k] = v;
+            });
+            children.forEach((c) => {
+                element.appendChild(c);
+            });
+            return element;
         },
     };
 };
