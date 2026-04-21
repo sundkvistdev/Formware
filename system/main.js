82377938c840e6d4a410cddb4663795754dc8cc4
sundkvistdev <lundish420flou@gmail.com>
2026-04-21 08:36:03 +0300

    Added mouse cursor variation test

--- system/main.js
+++ system/main.js
@@ -1,5 +1,27 @@
 async function main() {
-    console.log("Initialized!");
+    const root = document.querySelector(".formware.os-root");
+
+    const mouseCursors = [
+        "pointer-arrow",
+        "pointer-arrow-load",
+        "pointer-load",
+        "pointer-hand"
+    ];
+    const mouseCurTest = document.createElement("button");
+    mouseCurTest.classList.add("formware");
+    mouseCurTest.textContent = "HOVER";
+    
+    root.appendChild(mouseCurTest);
+
+    let mouseCurIdx = 0;
+    
+    setInterval(() => {
+        mouseCurTest.className = "formware " + mouseCursors[mouseCurIdx];
+        mouseCurIdx = (mouseCurIdx + 1) % mouseCursors.length;
+    }, 1000);
+
+
+    console.log("[Formware.System] Initialization Complete");
 }
 
 main();
